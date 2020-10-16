ebddb8dbbef5212805f444aa076d8ed95e559be7
Angelo Goncalves <agoncal7@my.centennialcollege.ca>
2020-10-16 12:00:09 -0400

    Added scripts + Sydney's reaper sprite
    Lemme know when the level is created so i can fix the dashing. Quick code, ignore it for now.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject followTarget;
+    private Vector3 targetPos;
+    public float moveSpeed;
+
+    private GameObject Player;
+
+    void Start()
+    {
+        //Checking if there's a player in the level already.
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+        //If a player is not null (is found), then grab that component.
+        if (Player != null)
+        {
+            followTarget = Player;
+        }
+    }
+    void Update()
+    {
+        targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Private
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private bool isFacingRight = true;
+    private bool isGrounded = false;
+
+    //Things to move the player.
+    [SerializeField] public float speed;
+    [SerializeField] Transform groundCheckPos;
+    [SerializeField] private LayerMask whatIsGround;
+    private float groundCheckRadius = 0.1f;
+
+    void Start()
+    {
+        //Grabbing components on the player to move and such.
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+    void FixedUpdate()
+    {
+        //Move the char left and right.
+        float horizMove = Input.GetAxis("Horizontal");
+        isGrounded = GroundCheck();
+        rb.velocity = new Vector2(horizMove * speed, rb.velocity.y);
+
+        if (isFacingRight && rb.velocity.x < 0 || (!isFacingRight && rb.velocity.x > 0))
+        {
+            Flip();
+        }
+    }
+    private void Flip()
+    {        
+        //Flip their sprite around depending on which way they're going.
+        Vector3 temp = transform.localScale;
+        temp.x *= -1;
+        transform.localScale = temp;
+
+        isFacingRight = !isFacingRight;
+    }
+    private bool GroundCheck()
+    {
+        return Physics2D.OverlapCircle(groundCheckPos.position, groundCheckRadius, whatIsGround);
+    }
+}
