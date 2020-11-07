94907ec34b59f14b3f0d80f14aceeee301cb4993
juneau <juneaugawat@hotmail.com>
2020-11-06 22:10:49 -0500

    UI buttons work. Death screen added. Levels actually playable
    Win text is based on the # of childs the candy object has instead of ui text now

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject startButton;
+    public GameObject settingsButton;
+    public GameObject quitButton;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
---
+++ Assets/Scripts/TempBoxRemover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempBoxRemover : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject box1;
+    [SerializeField]
+    private GameObject box2;
+
+    public int scoreValue = 1;
+    public GameController gameControllerScript;
+
+    private void Start()
+    {
+        //Checking if there's a Game Controller in the level already.
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+
+        //If a Game Controller is not null (is found), then grab that component.
+        if (gameControllerObject != null)
+        {
+            gameControllerScript = gameControllerObject.GetComponent<GameController>();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            gameControllerScript.AddScore(scoreValue);
+            box2.SetActive(false);
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            box1.SetActive(false);
+            Destroy(gameObject);
+        }
+    }
+}
