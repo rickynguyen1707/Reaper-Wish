03601f1223b6780243ebdebab98b5e484c74fe45
juneau <juneaugawat@hotmail.com>
2020-12-04 12:24:33 -0500

    Added credits after level 3.

---
+++ Assets/Scripts/GotoCredits.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GotoCredits : MonoBehaviour
+{
+    //Reference to the game controller.
+    public GameController gameControllerScript;
+    private void Start()
+    {
+        //Checking if there's a Game Controller in the level already.
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+
+        //If a Game Controller is not null (is found), then grab that component.
+        if (gameControllerObject != null)
+        {
+            gameControllerScript = gameControllerObject.GetComponent<GameController>();
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //If the gem collides with player, increase score and destroy it.
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(4);
+        }
+    }
+}
---
+++ Assets/Scripts/LeaveCredits.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class LeaveCredits : MonoBehaviour
+{
+    VideoPlayer video;
+
+    void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += CheckOver;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            SceneManager.LoadScene(0);
+    }
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
